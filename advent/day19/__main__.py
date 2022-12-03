6e0893836058ae19eb1608d964b2fe082fd776b6
Logan Gore <gorel@meta.com>
2022-12-03 14:35:16 -0800

    Add template to all days

---
+++ advent/day19/__main__.py
@@ -0,0 +1,11 @@
+#!/usr/bin/env python3
+
+from typing import Tuple
+
+from advent.utils import run_default
+
+def solve(input_file: str) -> Tuple[int, int]:
+    return (0, 0)
+
+
+run_default(__file__, solve)
